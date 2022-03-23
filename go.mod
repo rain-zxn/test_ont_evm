20a23883bcf5b663813a3a005524a4d79b613644
rain <206131521@qq.com>
2022-03-23 22:26:52 +0800

    prove

--- go.mod
+++ go.mod
@@ -9,10 +9,10 @@ require (
 	github.com/ontio/ontology v1.14.1-alpha
 	github.com/ontio/ontology-crypto v1.2.1
 	github.com/ontio/ontology-go-sdk v1.12.4
-	github.com/rain-zxn/ontology-go-sdk v1.0.0.fag
 	github.com/polynetwork/bridge-common v0.0.26
 	github.com/polynetwork/poly v1.3.1
 	github.com/polynetwork/poly-go-sdk v0.0.0-20210114035303-84e1615f4ad4
+	github.com/rain-zxn/ontology-go-sdk v1.1.1
 )
 
 replace (
--- ont_test.go
+++ ont_test.go
@@ -14,15 +14,14 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 	ontSDK "github.com/ontio/ontology-go-sdk"
 	ontcommon "github.com/ontio/ontology/common"
+	"github.com/ontio/ontology/core/types"
 	"github.com/ontio/ontology/merkle"
-	ontocccom "github.com/ontio/ontology/smartcontract/service/native/cross_chain/common"
-	ontSDK1Client "github.com/rain-zxn/ontology-go-sdk/client"
+	ccom "github.com/ontio/ontology/smartcontract/service/native/cross_chain/common"
 	ontnode "github.com/polynetwork/bridge-common/chains/ont"
 	polynode "github.com/polynetwork/bridge-common/chains/poly"
-	pcom "github.com/polynetwork/poly/common"
-	pplycommon "github.com/polynetwork/poly/native/service/cross_chain_manager/common"
-
-	bc "github.com/ontio/ontology/http/base/common"
+	polycommon "github.com/polynetwork/poly/common"
+	pcom "github.com/polynetwork/poly/native/service/cross_chain_manager/common"
+	ontSDK1Client "github.com/rain-zxn/ontology-go-sdk/client"
 
 	"strings"
 	"testing"
@@ -61,8 +60,7 @@ func HexReverse(arr []byte) []byte {
 	return x
 }
 
-
-func scan() (*msg.Tx){
+func scan() *msg.Tx {
 	sdk := ontSDK.NewOntologySdk()
 	client := sdk.NewRpcClient()
 	client.SetAddress("http://43.128.242.133:20336")
@@ -83,7 +81,7 @@ func scan() (*msg.Tx){
 	fmt.Println(eccmReversed)
 	flag := 0
 	for _, event0 := range events {
-		fmt.Println("event hash:",event0.TxHash)
+		fmt.Println("event hash:", event0.TxHash)
 		for _, notify := range event0.Notify {
 			if notify.ContractAddress == eccmReversed {
 				flag++
@@ -98,7 +96,7 @@ func scan() (*msg.Tx){
 					err = fmt.Errorf("decoding states err:%v", err)
 					return nil
 				}
-				source := pcom.NewZeroCopySource(data)
+				source := polycommon.NewZeroCopySource(data)
 				var storageLog StorageLog
 				err = storageLog.Deserialization(source)
 				if err != nil {
@@ -117,7 +115,6 @@ func scan() (*msg.Tx){
 				}
 				fmt.Println("CrossChainEvent", event)
 
-
 				tx := &msg.Tx{
 					TxType:     msg.SRC,
 					TxId:       msg.EncodeTxId(event.TxId),
@@ -129,7 +126,7 @@ func scan() (*msg.Tx){
 					SrcProxy:   event.ProxyOrAssetContract.String(),
 					DstProxy:   common.BytesToAddress(event.ToContract).String(),
 				}
-				jsontx,_:=json.Marshal(tx)
+				jsontx, _ := json.Marshal(tx)
 				fmt.Println(string(jsontx))
 				return tx
 			}
@@ -144,16 +141,16 @@ type StorageLog struct {
 	Data    []byte
 }
 
-func (self *StorageLog) Serialization(sink *pcom.ZeroCopySink) {
-	sink.WriteAddress(pcom.Address(self.Address))
+func (self *StorageLog) Serialization(sink *polycommon.ZeroCopySink) {
+	sink.WriteAddress(polycommon.Address(self.Address))
 	sink.WriteUint32(uint32(len(self.Topics)))
 	for _, t := range self.Topics {
-		sink.WriteHash(pcom.Uint256(t))
+		sink.WriteHash(polycommon.Uint256(t))
 	}
 	sink.WriteVarBytes(self.Data)
 }
 
-func (self *StorageLog) Deserialization(source *pcom.ZeroCopySource) error {
+func (self *StorageLog) Deserialization(source *polycommon.ZeroCopySource) error {
 	address, _ := source.NextAddress()
 	self.Address = common.Address(address)
 	l, _ := source.NextUint32()
@@ -167,9 +164,9 @@ func (self *StorageLog) Deserialization(source *pcom.ZeroCopySource) error {
 		return fmt.Errorf("StorageLog.Data eof")
 	}
 	self.Data = data
-
 	return nil
 }
+
 type PolyChainListen struct {
 	polySdk *chainsdk.PolySDKPro
 }
@@ -181,10 +178,9 @@ func NewPolyChainListen(urls []string) *PolyChainListen {
 	return polyListen
 }
 
-
 type MakeTxParamWithSender struct {
 	Sender ontcommon.Address
-	ontocccom.MakeTxParam
+	ccom.MakeTxParam
 }
 
 var (
@@ -196,7 +192,13 @@ var (
 	}
 )
 
-func (this *MakeTxParamWithSender) Serialization() (data []byte, err error) {
+//func (this *MakeTxParamWithSender) Serialization() (data []byte, err error) {
+//	sink := ontcommon.NewZeroCopySink(nil)
+//	this.MakeTxParam.Serialization(sink)
+//	data, err = arguments.Pack(this.Sender, sink.Bytes())
+//	return
+//}
+func (this *MakeTxParamWithSender) Serialization1() (data []byte, err error) {
 	sink := ontcommon.NewZeroCopySink(nil)
 	sink.WriteAddress(ontcommon.Address(this.Sender))
 	this.MakeTxParam.Serialization(sink)
@@ -205,87 +207,78 @@ func (this *MakeTxParamWithSender) Serialization() (data []byte, err error) {
 }
 
 func Compose(tx *msg.Tx) (err error) {
-	polyNode:=polynode.New("http://40.115.182.238:40336")
-	ontNode:=ontnode.New("http://43.128.242.133:20336")
+	polyNode := polynode.New("http://40.115.182.238:40336")
+	ontNode := ontnode.New("http://43.128.242.133:20336")
 	v, err := polyNode.GetSideChainMsg(uint64(5555), tx.SrcHeight)
 	if err != nil {
-		fmt.Println("err GetSideChainMsg",err)
-		return  fmt.Errorf("GetSideChainMsg:%s", err)
+		fmt.Println("err GetSideChainMsg", err)
+		return fmt.Errorf("GetSideChainMsg:%s", err)
 	}
 	if len(v) == 0 {
 		msg, err := ontNode.GetCrossChainMsg(uint32(tx.SrcHeight))
 		if err != nil {
-			fmt.Println("err ontNode.GetCrossChainMsg",err)
+			fmt.Println("err ontNode.GetCrossChainMsg", err)
 			return err
 		}
+		fmt.Println("GetSideChainMsg:", msg)
 		tx.SrcStateRoot, err = hex.DecodeString(msg)
 		if err != nil {
-			fmt.Println("err hex.DecodeString(msg)",err)
+			fmt.Println("err hex.DecodeString(msg)", err)
 			return err
 		}
 	}
 	var clientMgr ontSDK1Client.ClientMgr
 	clientMgr.NewRpcClient().SetAddress("http://43.128.242.133:20336")
-	hashes, err :=clientMgr.GetCrossStatesLeafHashes(float64(tx.SrcHeight))
+	hashes, err := clientMgr.GetCrossStatesLeafHashes(float64(tx.SrcHeight))
 	if err != nil {
-		fmt.Println("err GetCrossStatesLeafHashes",err)
-		return  fmt.Errorf("GetCrossStatesLeafHashes:%s", err)
+		fmt.Println("err GetCrossStatesLeafHashes", err)
+		return fmt.Errorf("GetCrossStatesLeafHashes:%s", err)
 	}
-	fmt.Println("hashes.Hashes:",hashes.Hashes)
 	eccmAddr := "34d4a23A1FC0C694f0D74DDAf9D8d564cfE2D430"
-	eccmAddr =HexStringReverse1(eccmAddr)
-	param := ontocccom.MakeTxParam{}
-	par,_:=hex.DecodeString(tx.SrcParam)
+	eccmAddr = HexStringReverse1(eccmAddr)
+	param := ccom.MakeTxParam{}
+	par, _ := hex.DecodeString(tx.SrcParam)
 	err = param.Deserialization(ontcommon.NewZeroCopySource(par))
 	if err != nil {
 		fmt.Println("err param.Deserialization:", err)
 		return err
 	}
+	fmt.Println("ontcommon.ToHexString(param.TxHash):", ontcommon.ToHexString(param.TxHash))
+	ontEccmAddr, err := ontcommon.AddressFromHexString(eccmAddr)
+	fmt.Println("ontEccmAddr", ontEccmAddr, ontEccmAddr.ToHexString())
 
-	ontEccmAddr,err:=ontcommon.AddressFromHexString(eccmAddr)
-	makeTxParamWithSender:=&MakeTxParamWithSender{
+	makeTxParamWithSender := &MakeTxParamWithSender{
 		ontEccmAddr,
 		param,
 	}
-	itemValue,err:=makeTxParamWithSender.Serialization()
+	itemValue, err := makeTxParamWithSender.Serialization1()
 	if err != nil {
 		fmt.Println("err makeTxParamWithSender.Serialization:", err)
 		return err
 	}
-	hashesx:=make([]ontcommon.Uint256,0)
-	for k,v:=range hashes.Hashes{
-		fmt.Println("hashes.Hashes[",k,"] : ",v)
-		uint256v,_:=ontcommon.Uint256FromHexString(v)
-		hashesx=append(hashesx,uint256v)
+	hashesx := make([]ontcommon.Uint256, 0)
+	for _, v := range hashes.Hashes {
+		uint256v, _ := ontcommon.Uint256FromHexString(v)
+		hashesx = append(hashesx, uint256v)
 	}
-	fmt.Println("itemValue:",ontcommon.ToHexString(itemValue))
-	fmt.Println("hashesx:",hashesx)
 	path, err := merkle.MerkleLeafPath(itemValue, hashesx)
 	if err != nil {
 		fmt.Println("err  merkle.MerkleLeafPath:", err)
 		return err
 	}
-	proof := &bc.CrossStatesProof{}
-	err = json.Unmarshal(path, proof)
-	if err != nil {
-		fmt.Println("err json.Unmarshal(path, proof)",err)
-		return err
-	}
-	tx.SrcProof, err = hex.DecodeString(proof.AuditPath)
-	if err != nil {
-		return
-	}
+	fmt.Println("string(path):", ontcommon.ToHexString(path))
+	tx.SrcProof = path
 	{
 		value, _, _, _ := msg.ParseAuditPath(tx.SrcProof)
 		if len(value) == 0 {
 			return fmt.Errorf("ParseAuditPath got null param")
 		}
-		param := &ontocccom.MakeTxParam{}
+		param := &ccom.MakeTxParam{}
 		err = param.Deserialization(ontcommon.NewZeroCopySource(value))
 		if err != nil {
 			return
 		}
-		tx.Param = &pplycommon.MakeTxParam{
+		tx.Param = &pcom.MakeTxParam{
 			TxHash:              param.TxHash,
 			CrossChainID:        param.CrossChainID,
 			FromContractAddress: param.FromContractAddress,
@@ -295,15 +288,32 @@ func Compose(tx *msg.Tx) (err error) {
 			Args:                param.Args,
 		}
 	}
-	return
-}
 
+	proof := tx.SrcProof
+	source := ontcommon.NewZeroCopySource(tx.SrcStateRoot)
+	crossChainMsg := new(types.CrossChainMsg)
+	err = crossChainMsg.Deserialization(source)
+	if err != nil {
+		fmt.Println("err Deserialization(source):", err)
+	}
+	v, err = merkle.MerkleProve(proof, crossChainMsg.StatesRoot)
+	if err != nil {
+		fmt.Println("err MerkleProve(proof, crossChainMsg.StatesRoot):", err)
+	}
+	fmt.Println("v:", ontcommon.ToHexString(v))
+	return nil
+}
 
 //-tags testnet
 //github.com/onto/ontogo-sdk v1.0.0 => /Users/rain/go/gopath/src/github.com/me/fork-onto-go-sdk/ontology-go-sdk
 func Test_ontCrossTx(t *testing.T) {
-	tx:=scan()
+	tx := scan()
 	fmt.Println(tx.SrcHeight)
-	err:=Compose(tx)
-	fmt.Println("end err:",err)
+	err := Compose(tx)
+	fmt.Println("end err:", err)
+}
+
+func Test_ontCrossT1(t *testing.T) {
+	s := "34d4a23A1FC0C694f0D74DDAf9D8d564cfE2D430"
+	fmt.Println(HexStringReverse1(s))
 }
